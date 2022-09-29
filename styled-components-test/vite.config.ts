ce8a7ca22702d691af5a378d16a57ce80fa085c2
IlYeong Jeong <developjiy@gmail.com>
2022-09-30 00:04:35 +0900

    ref #4 styled components 기본 환경 셋업

--- styled-components-test/vite.config.ts
+++ styled-components-test/vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import macrosPlugin from 'vite-plugin-babel-macros';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), macrosPlugin()]
+  server: {
+    port: 5501,
+    host: '0.0.0.0',
+  },
+  plugins: [
+    react(),
+    macrosPlugin()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
